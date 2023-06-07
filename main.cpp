98fbca91f105306d7e70aa8fd484a2ce1865b570
KIMYOUNGGON <dudrhs950@gmail.com>
2023-06-07 10:42:25 +0900

    if문 예제3

--- main.cpp
+++ main.cpp
@@ -1,19 +1,40 @@
-/* 합격? 불합격? */
+/* 크기 비교하기 */
 #include <stdio.h>
 
 int main() {
-    int score;
+    int i, j;
 
-    printf("당신의 수학 점수를 입력 하세요! : ");
-    scanf("%d", &score);
+    printf("크기를 비교할 두 수를 입력해 주세요 : ");
+    scanf("%d %d", &i, &j);
 
-    if (score >= 90) {
-        printf("당신은 합격 입니다! \n");
+    if (i > j)  // i 가 j 보다 크면
+    {
+        printf("%d 는 %d 보다 큽니다 \n", i, j);
     }
 
-    if (score < 90) {
-        printf("당신은 불합격 입니다! \n");
+    if (i < j)  // i 가 j 보다 작으면
+    {
+        printf("%d 는 %d 보다 작습니다 \n", i, j);
     }
 
+    if (i >= j)  // i 가 j 보다 크거나 같으면
+    {
+        printf("%d 는 %d 보다 크거나 같습니다 \n", i, j);
+    }
+
+    if (i <= j)  // i 가 j 보다 작거나 같으면
+    {
+        printf("%d 는 %d 보다 작거나 같습니다 \n", i, j);
+    }
+
+    if (i == j)  // i 와 j 가 같으면
+    {
+        printf("%d 는 %d 와(과) 같습니다 \n", i, j);
+    }
+
+    if (i != j)  // i 와 j 가 다르면
+    {
+        printf("%d 는 %d 와(과) 다릅니다 \n", i, j);
+    }
     return 0;
 }

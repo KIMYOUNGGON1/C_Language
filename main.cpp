c150c347e1f34ff25054d964eb1520d0fbae4958
KIMYOUNGGON <dudrhs950@gmail.com>
2023-06-07 17:20:37 +0900

    or 논리연산자

--- main.cpp
+++ main.cpp
@@ -2,9 +2,12 @@
 #include <stdio.h>
 
 int main() {
-    int a = 31, b = 15;
-    printf("a & b = %d\n", a & b);
-    printf("a && b = %d\n", a && b);
+    int height, weight;
+    printf("당신의 키와 몸무게를 각각 입력해 주세요 : ");
+    scanf("%d %d", &height, &weight);
 
+    if (height >= 190 || weight >= 100) {
+        printf("당신은 '거구' 입니다. \n");
+    }
     return 0;
 }

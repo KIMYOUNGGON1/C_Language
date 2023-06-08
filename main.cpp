bbe4535a995af9a1dc0b19ec5cecab24dcff9db6
KIMYOUNGGON <dudrhs950@gmail.com>
2023-06-08 12:18:03 +0900

    중첩 for문 예시

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@ int main() {
     int i, j;
 
     for (i = 1; i < 10; i++) {
-        for (j = 1; j < 10; j++) {
-            printf(" %d x %d = %d \n", i, j, i * j);
+        for (j = 1; j < i; j++) {
+            printf("%d ", j);
         }
     }
     return 0;

6fe53e396ecc49b6d9ba0e39ebd4143c347bcd20
KIMYOUNGGON <dudrhs950@gmail.com>
2023-06-08 10:48:22 +0900

    for문

--- main.cpp
+++ main.cpp
@@ -2,15 +2,9 @@
 #include <stdio.h>
 
 int main() {
-    int height, weight;
-    printf("당신의 키와 몸무게를 각각 입력해 주세요 : ");
-    scanf("%d %d", &height, &weight);
-
-    if (height >= 190 || weight >= 100) {
-        printf("당신은 '거구' 입니다. \n");
-    }
-    if (!(height >= 190 || weight >= 100)) {
-        printf("당신은 거구가 아닙니다. \n");
+    int i;
+    for (i = 0; i < 20; i++) {
+        printf("숫자 : %d \n", i);
     }
     return 0;
 }

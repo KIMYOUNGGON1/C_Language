48ec4e3fab25cf76463fdc41e3b05d09a8460a71
KIMYOUNGGON <dudrhs950@gmail.com>
2023-06-08 12:13:55 +0900

    break문 예시

--- main.cpp
+++ main.cpp
@@ -1,18 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    int user_answer;
-
-    printf("컴퓨터가 생각한 숫자를 맞추어 보세요! \n");
-
     for (;;) {
-        scanf("%d", &user_answer);
-        if (user_answer == 3) {
-            printf("맞추셨군요! \n");
-            break;
-        } else {
-            printf("틀렸어요! \n");
-        }
+        break;
+        printf("a");
     }
     return 0;
 }

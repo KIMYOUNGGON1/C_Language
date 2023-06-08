e200d73fd53ed8ea1f632acafcb17768ede392fe
KIMYOUNGGON <dudrhs950@gmail.com>
2023-06-08 12:11:18 +0900

    for문 예시

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
-/* 논리 연산자 */
 #include <stdio.h>
 
 int main() {
-    int i, sum = 0;
-    for (i = 0; i <= 10000; ++i) {
-        sum = sum + i;
+    int i;
+    int subject, score;
+    double sum_score = 0;
+
+    printf("몇 개의 과목 점수를 입력 받을 것인가요?");
+    scanf("%d", &subject);
+
+    printf("\n 각 과목의 점수를 입력해 주세요 \n");
+    for (i = 1; i <= subject; i++) {
+        printf("과목 %d : ", i);
+        scanf("%d", &score);
+        sum_score = sum_score + score;
     }
-    printf("1 부터 10000 까지의 합 : %d \n", sum);
+
+    printf("전체 과목의 평균은 : %.2f \n", sum_score / subject);
     return 0;
 }

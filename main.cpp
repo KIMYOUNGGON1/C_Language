50e71b7f01ac304589c2a3ed11ba43cf591548f4
KIMYOUNGGON <dudrhs950@gmail.com>
2023-06-08 12:13:14 +0900

    break문

--- main.cpp
+++ main.cpp
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    int i;
-    int subject, score;
-    double sum_score = 0;
+    int user_answer;
 
-    printf("몇 개의 과목 점수를 입력 받을 것인가요?");
-    scanf("%d", &subject);
+    printf("컴퓨터가 생각한 숫자를 맞추어 보세요! \n");
 
-    printf("\n 각 과목의 점수를 입력해 주세요 \n");
-    for (i = 1; i <= subject; i++) {
-        printf("과목 %d : ", i);
-        scanf("%d", &score);
-        sum_score = sum_score + score;
+    for (;;) {
+        scanf("%d", &user_answer);
+        if (user_answer == 3) {
+            printf("맞추셨군요! \n");
+            break;
+        } else {
+            printf("틀렸어요! \n");
+        }
     }
-
-    printf("전체 과목의 평균은 : %.2f \n", sum_score / subject);
     return 0;
 }

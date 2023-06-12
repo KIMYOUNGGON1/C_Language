dccc5e18526e925671da545ce8758a85a250877f
KIMYOUNGGON <dudrhs950@gmail.com>
2023-06-12 10:32:36 +0900

    for문 예제

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
 #include <stdio.h>
 
+//N 줄인 삼각형을 출력한다. 단, 사용자 로부터 임의의 N 을 입력 받는다. 아래는 N = 3 일 때의 출력 예시 이다.
+
 int main() {
-    int i = 1, sum = 0;
+    int i, j, n, k = 0;
+    scanf("%d",&n);
 
-    do {
-        sum += i;
-        i++;
-    } while (i < 1);
+    for(i = 1; i <= n; ++i, k = 0)
+    {
+        for(j = 1; j <= n-i; ++j)
+        {
+            printf(" ");
+        }
 
-    printf(" sum : %d \n", sum);
+        while(k != 2 * i-1)
+        {
+            printf("*");
+            ++k;
+        }
+
+        printf("\n");
+    }
+
     return 0;
 }
